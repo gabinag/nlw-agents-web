ebf374bdbc5ec0e61068f4bc57ed81a0f82eb3a4
Gabriela Nagamuta <gabrielanagamuta@timepix.com.br>
2025-07-09 16:09:45 -0300

    feat: app setup, rooms listing

---
+++ src/pages/room.tsx
@@ -0,0 +1,15 @@
+import { Navigate, useParams } from 'react-router-dom';
+
+type RoomsParams = {
+    roomId: string
+}
+
+export function Room() {
+    const params = useParams<RoomsParams>()
+
+    if(!params.roomId) {
+        return <Navigate replace to="/" />
+    }
+
+    return <div>Room Details {JSON.stringify(params)}</div>
+}
